d888b8901bb59afe6bcb61f18da9353df587b78f
Jasen <jasen.miyamoto@gmail.com>
2024-01-18 13:55:46 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+    const messages =[]
+    for (let i = 0; i < names.length; i++) {
+        debugger;
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return(messages);
+}
+writeCards(names, "surprise");
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n--)
+    }
+}
+countDown(10);
